52537bc0a3c41c572e0c3e14da4b71fd28ebcc9a
DevsSRO <webbuilder.jobcloud@gmail.com>
2023-04-16 20:06:47 +0300

    main

--- help.php
+++ help.php
@@ -1,12 +1,16 @@
+<h2>
+    NEWWWW
+</h2>
 <div class="date-container">
     <p>
         - это текущая дата!
     </p>
 </div>
-<div class="old">
-    <p class="error">error</p>
-</div>
 <div class="new">
+    <div class="old 67889">
+        <p class="error">error</p>
+    </div>
+
     <p class="main-title title title_m">
         <strong>
             <?= date("d-m-Y"); ?>
