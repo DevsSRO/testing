34c1e730b23ed4677bb90d5ff8a81dfd07f3db9d
Anastasia Lebedeva <kisma6@mail.ru>
2023-04-16 19:59:06 +0300

    fff

--- help.php
+++ help.php
@@ -1,12 +1,13 @@
-<div class="date-container">
-    <p>
-        - это текущая дата!
-    </p>
-</div>
-<div class="new">
+<div class="new super">
+    <p>Новая строка</p>
     <p class="main-title title title_m">
         <strong>
             <?= date("d-m-Y"); ?>
         </strong>
+    </p>
+</div>
+<div class="date-container">
+    <p>
+        - это текущая дата!
     </p>
 </div>
