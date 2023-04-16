8f49a8b08ae270df09cdbede3a75de90da564851
Anastasia Lebedeva <kisma6@mail.ru>
2023-04-16 20:08:06 +0300

    1234545

--- help.php
+++ help.php
@@ -1,16 +1,12 @@
-<h2>
-    NEWWWW
-</h2>
 <div class="date-container">
     <p>
         - это текущая дата!
     </p>
+    <h2>
+        NEWWWW
+    </h2>
 </div>
 <div class="new">
-    <div class="old 67889">
-        <p class="error">error</p>
-    </div>
-
     <p class="main-title title title_m">
         <strong>
             <?= date("d-m-Y"); ?>
