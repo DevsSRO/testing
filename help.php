564564b03be1a5fd6a4a0aa7e9d22d17e2b54c3e
Anastasia Lebedeva <kisma6@mail.ru>
2023-04-16 20:05:25 +0300

    123456

--- help.php
+++ help.php
@@ -1,13 +1,15 @@
-<div class="new super">
-    <p>Новая строка</p>
+<div class="date-container">
+    <p>
+        - это текущая дата!
+    </p>
+</div>
+<div class="old">
+    <p class="error">error</p>
+</div>
+<div class="new">
     <p class="main-title title title_m">
         <strong>
             <?= date("d-m-Y"); ?>
         </strong>
-    </p>
-</div>
-<div class="date-container">
-    <p>
-        - это текущая дата!
     </p>
 </div>
